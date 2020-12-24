c505c540b4ec688fcf58109af8ffcfa8444fcca1
andrew-curthoys <andrew@curthoys.io>
2020-12-24 11:25:03 -0600

    Updated `crawler.py` to account for moving all Selenium functionality to `helpers.py`

---
+++ crawler.py
@@ -0,0 +1,24 @@
+import argparse
+from datetime import datetime
+import logging
+from helpers import Crawler
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--start_index', help='The start index of the movie list you would like to pull', type=int)
+parser.add_argument('--end_index', help='The end index of the movie list you would like to pull', type=int)
+args = parser.parse_args()
+start_index = args.start_index
+end_index = args.end_index
+
+logging.basicConfig(
+    # filename=f'./logs/movie_crawler_log-{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}.log',
+    filename=f'./logs/movie_crawler_log-{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}.log',
+    format='%(asctime)s: %(levelname)s - %(name)s - %(module)s %(funcName)s, line %(lineno)d: %(message)s',
+    datefmt='%m/%d/%Y %H:%M:%S',
+    level=logging.INFO
+)
+
+
+if __name__ == "__main__":
+    crawler = Crawler(start_index=start_index, end_index=end_index)
+    crawler.get_all_movie_data()
